b4f44baaa3eb472d19335e14b2a5a67d8a71c94b
Baloou <127010557+Marco-Jan@users.noreply.github.com>
2024-02-07 17:51:40 +0100

    beta open 0.70224_3

--- src/Components/Background.tsx
+++ src/Components/Background.tsx
@@ -3,9 +3,9 @@ import PokeMind_Background from '../img/PokeMind_Background.jpg'
 export const Background = () => {
     return (
         <>
-        
             <img src={PokeMind_Background} className="background" alt="PokeMind background" />
-
         </>
     )
+
+    //comment
 }
